773f39141be5a2e367167f12aa459432e97892f4
ValentijnBraun <127754182+ValentijnBraun@users.noreply.github.com>
2023-04-06 17:22:04 +0200

    Added scraping of critic reviews and user reviews

--- metacritic_spider.py
+++ metacritic_spider.py
@@ -13,6 +13,8 @@ class MetacriticSpider(scrapy.Spider):
                 if detail_url:
                     detail_url = response.urljoin(detail_url)
                     yield scrapy.Request(url=detail_url,callback=self.parseDetail)
+                    yield scrapy.Request(url=(detail_url+'/critic-reviews'),callback=self.parse_critic_reviews)
+                    yield scrapy.Request(url=(detail_url+'/user-reviews'),callback=self.parse_user_reviews)
                 
         next_page = response.css('a[rel="next"]::attr(href)').extract_first()
         if next_page:
@@ -41,3 +43,42 @@ class MetacriticSpider(scrapy.Spider):
             }
         
         yield item
+
+    def parse_critic_reviews(self, response):
+        for review in response.css('li.review.critic_review'):
+            name_path = 'div.source a::text'
+            if review.css(name_path).extract_first() == None:
+                name_path = 'div.source::text'
+            
+            critic_reviews = {
+                'game':response.css('div.product_title h1::text').extract_first(),
+                'platform':response.css('div.product_title span a::text').extract_first().strip(),
+                'critic_name':review.css(name_path).extract_first(),
+                'grade':review.css('div.metascore_w::text').extract_first(),
+                'review':review.css('div.review_body::text').extract_first().strip(),
+                'date':review.css('div.date::text').extract_first()
+            }
+            yield critic_reviews
+
+    def parse_user_reviews(self, response):
+        for review in response.css('li.review.user_review'):
+            name_path = 'div.name a::text'
+            if review.css(name_path).extract_first().strip() == None or "":
+                name_path = 'div.name::text'
+            
+            user_reviews = {
+                'game':response.css('div.product_title h1::text').extract_first(),
+                'platform':response.css('div.product_title span a::text').extract_first().strip(),
+                'username':review.css(name_path).extract_first(),
+                'grade':review.css('div.metascore_w::text').extract_first(),
+                'review':review.css('div.review_body span::text').extract_first().strip(),
+                'date':review.css('div.date::text').extract_first(),
+                'thumbs_helpful':review.css('div.review_helpful span.total_ups::text').extract_first(),
+                'thumbs_total':review.css('div.review_helpful span.total_thumbs::text').extract_first()
+            }
+            yield user_reviews
+        
+        next_user_review_page = response.css('a[rel="next"]::attr(href)').extract_first()
+        if next_user_review_page:
+            next_user_review_page = response.urljoin(next_user_review_page)
+            yield scrapy.Request(url=next_user_review_page,callback=self.parse_user_reviews)
